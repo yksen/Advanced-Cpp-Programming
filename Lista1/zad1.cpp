d64276fce8bd2baac65c441528c86ef212a44e39
xen <selegakamil@gmail.com>
2022-10-11 22:54:28 +0200

    Add template functions tests

--- Lista1/zad1.cpp
+++ Lista1/zad1.cpp
@@ -10,19 +10,21 @@ namespace cpplab
     }
 
     template <typename T, typename U, typename V>
-    auto alias(T a, U b, V c) -> decltype(c(a, b))
+    auto alias(T a, U b, V f) -> decltype(f(a, b))
     {
-        return c(a, b);
+        return f(a, b);
     }
 }
 
 int main()
 {
     std::cout << cpplab::add(5, 3.14f) << "\n";
-    std::cout << cpplab::add<std::string, std::string>("hello", "world") << "\n";
+    std::cout << cpplab::add('d', 4) << "\n";
+    std::cout << cpplab::add<std::string, std::string>("Hello", "world") << "\n";
 
-    std::cout << cpplab::alias(5, 4, cpplab::add<int, int>) << "\n";
-    std::cout << cpplab::alias(12.4f, 7.7f, [](auto a, auto b) { return a > b ? a : b; }) << "\n";
+    std::cout << cpplab::alias(5, 9, cpplab::add<int, int>) << "\n";
+    std::cout << cpplab::alias(12.4f, 7.7, [](auto a, auto b) { return a > b ? a : b; }) << "\n";
+    std::cout << cpplab::alias("Hello", "world", [](auto a, auto b) { return std::strcmp(a, b) > 0 ? a : b; }) << "\n";
 
     return 0;
 }
