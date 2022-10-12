376ace25a479987dfd81c650b450cade35236008
yksen <selegakamil@gmail.com>
2022-10-12 11:13:01 +0200

    Extract reallocate and add resize

--- Lista1/zad2.cpp
+++ Lista1/zad2.cpp
@@ -26,14 +26,7 @@ namespace cpplab
         void push_back(T item)
         {
             if (size >= capacity)
-            {
-                size_t new_capacity = (capacity == 0) ? 1 : 2 * capacity;
-                T *new_data = new T[new_capacity];
-                for (size_t i = 0; i < size; ++i)
-                    new_data[i] = data[i];
-                data.reset(new_data);
-                capacity = new_capacity;
-            }
+                reallocate();
             data[size] = item;
             ++size;
         }
@@ -41,6 +34,19 @@ namespace cpplab
         {
             if (size > 0)
                 --size;
+        }
+        void resize(size_t new_size, T value)
+        {
+            if (new_size > size && new_size <= capacity)
+                for (size_t i = size; i < new_size; ++i)
+                    data[i] = value;
+            else if (new_size > capacity)
+            {
+                reallocate();
+                for (size_t i = size; i < new_size; ++i)
+                    data[i] = value;
+            }
+            size = new_size;
         }
         void print()
         {
@@ -52,16 +58,28 @@ namespace cpplab
         size_t capacity;
         size_t size;
         std::unique_ptr<T[]> data;
+
+        void reallocate()
+        {
+            size_t new_capacity = (capacity == 0) ? 1 : 2 * capacity;
+            T *new_data = new T[new_capacity];
+            for (size_t i = 0; i < size; ++i)
+                new_data[i] = data[i];
+            data.reset(new_data);
+            capacity = new_capacity;
+        }
     };
 }
 
 int main()
 {
-    cpplab::vector<int> v;
+    cpplab::vector<int> int_vector;
+    cpplab::vector<std::string> string_vector;
 
     for (int i = 0; i < 10; ++i)
-        v.push_back(i);
-    v.print();
+        int_vector.push_back(i);
+    int_vector.resize(20, 0);
+    int_vector.print();
 
     return 0;
 }
