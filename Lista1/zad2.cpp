49ff964a34a0bd471102ef40481b42e099c9a6cb
xen <selegakamil@gmail.com>
2022-10-12 21:27:28 +0200

    Add size getter to fix  dot product for any types

--- Lista1/zad2.cpp
+++ Lista1/zad2.cpp
@@ -11,84 +11,90 @@ namespace cpplab
         typedef T value_type;
         vector()
         {
-            capacity = 0;
-            size = 0;
+            capacity_ = 0;
+            size_ = 0;
         }
         vector(std::initializer_list<T> list)
         {
-            capacity = list.size();
-            size = list.size();
-            data = std::make_unique<T[]>(capacity);
-            std::copy(list.begin(), list.end(), data.get());
+            capacity_ = list.size();
+            size_ = list.size();
+            data_ = std::make_unique<T[]>(capacity_);
+            std::copy(list.begin(), list.end(), data_.get());
         }
         ~vector()
         {
-            data.release();
+            data_.release();
         }
         T &operator[](size_t index)
         {
-            if (index < 0 || index >= size)
+            if (index < 0 || index >= size_)
                 throw std::out_of_range("vector index out of bounds");
-            return data[index];
+            return data_[index];
         }
         T operator[](size_t index) const
         {
-            if (index < 0 || index >= size)
+            if (index < 0 || index >= size_)
                 throw std::out_of_range("vector index out of bounds");
-            return data[index];
-        }
-        double operator*(vector<T> const &rhs) const
-        {
-            double result = 0;
-            size_t min_size = size < rhs.size ? size : rhs.size;
-            for (size_t i = 0; i < min_size; i++)
-                result += data[i] * rhs[i];
-            return result;
+            return data_[index];
         }
         friend std::ostream &operator<<(std::ostream &os, const vector<T> &v)
         {
-            for (int i = 0; i < v.size; ++i)
+            for (size_t i = 0; i < v.size_; ++i)
                 os << v[i] << " ";
             return os;
         }
 
+        template <typename U>
+        auto operator*(vector<U> const &rhs)
+        {
+            size_t min_size = size_ < rhs.size() ? size_ : rhs.size();
+            decltype(data_[0] * rhs[0]) result = 0;
+            for (size_t i = 0; i < min_size; i++)
+                result += data_[i] * rhs[i];
+            return result;
+        }
+
         void push_back(T item)
         {
-            if (size >= capacity)
+            if (size_ >= capacity_)
                 reallocate();
-            data[size] = item;
-            ++size;
+            data_[size_] = item;
+            ++size_;
         }
         void pop_back()
         {
-            if (size > 0)
-                --size;
+            if (size_ > 0)
+                --size_;
         }
         void resize(size_t new_size, T value)
         {
-            if (new_size > size && new_size <= capacity)
-                for (size_t i = size; i < new_size; ++i)
-                    data[i] = value;
-            else if (new_size > capacity)
-                for (size_t i = size; i < new_size; ++i)
+            if (new_size > size_ && new_size <= capacity_)
+                for (size_t i = size_; i < new_size; ++i)
+                    data_[i] = value;
+            else if (new_size > capacity_)
+                for (size_t i = size_; i < new_size; ++i)
                     this->push_back(value);
-            size = new_size;
+            size_ = new_size;
+        }
+        size_t size() const
+        {
+            return size_;
         }
 
     private:
-        size_t capacity;
-        size_t size;
-        std::unique_ptr<T[]> data;
-
         void reallocate()
         {
-            size_t new_capacity = (capacity == 0) ? 1 : 2 * capacity;
+            size_t new_capacity = (capacity_ == 0) ? 1 : 2 * capacity_;
             T *new_data = new T[new_capacity];
-            for (size_t i = 0; i < size; ++i)
-                new_data[i] = data[i];
-            data.reset(new_data);
-            capacity = new_capacity;
+            for (size_t i = 0; i < size_; ++i)
+                new_data[i] = data_[i];
+            data_.reset(new_data);
+            capacity_ = new_capacity;
         }
+
+        size_t size_;
+        size_t capacity_;
+        std::unique_ptr<T[]> data_;
     };
 }
 
@@ -107,9 +113,11 @@ int main()
     string_vector.resize(5, "world");
     std::cout << string_vector << std::endl;
 
-    cpplab::vector<int> a{1, 2, 3};
+    cpplab::vector<int> a{1, 3, 2};
     cpplab::vector<int> b{4, 5, 6, 7};
+    cpplab::vector<double> c{1.5, 1. / 3, 0.5};
     std::cout << a * b << std::endl;
+    std::cout << a * c << std::endl;
 
     return 0;
 }
