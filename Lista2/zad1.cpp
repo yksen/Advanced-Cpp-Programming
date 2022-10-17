179f4472d4cd9cbfdb2576ef00e41db2e8d9fa64
xen <selegakamil@gmail.com>
2022-10-18 00:07:41 +0200

    Overload cpplab::add for two const char* args

--- Lista2/zad1.cpp
+++ Lista2/zad1.cpp
@@ -15,13 +15,22 @@ namespace cpplab
         return *a + *b;
     }
 
+    auto add(const char *a, const char *b) -> const char *
+    {
+        char *result = new char[strlen(a) + strlen(b)];
+        strcpy(result, a);
+        strcat(result, b);
+        return result;
+    }
 }
 
 int main()
 {
     int a = 2, b = 5;
+    const char *c = "Hello", *d = "World";
 
     std::cout << cpplab::add(&a, &b) << std::endl;
+    std::cout << cpplab::add(c, d) << std::endl;
 
     return 0;
 }
