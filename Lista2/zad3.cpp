b5e11efd4dd2f0a5faae9fd4418c0a9dfff09526
xen <selegakamil@gmail.com>
2022-10-20 16:52:01 +0200

    Fix add_total function

--- Lista2/zad3.cpp
+++ Lista2/zad3.cpp
@@ -2,19 +2,19 @@
 
 namespace cpplab
 {
-    int add_total() { return 0; }
-
     template <typename T, typename... Ts>
     auto add_total(T head, Ts... tail)
     {
-        return head + add_total(tail...);
+        if constexpr (sizeof...(tail) == 0)
+            return head;
+        else
+            return head + add_total(tail...);
     }
 
 }
 
 int main()
 {
-    std::cout << cpplab::add_total() << std::endl;
     std::cout << cpplab::add_total(5) << std::endl;
     std::cout << cpplab::add_total(2.5, 6) << std::endl;
     std::cout << cpplab::add_total(1, 1.0, 1.0f) << std::endl;
