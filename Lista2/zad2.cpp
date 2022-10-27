1e177b0d6dca9e58b3152ac5afc629204ce77870
xen <selegakamil@gmail.com>
2022-10-27 16:07:37 +0200

    Remove the constructor and pass the parameters in a template

--- Lista2/zad2.cpp
+++ Lista2/zad2.cpp
@@ -2,33 +2,27 @@
 
 namespace cpplab
 {
-    template <class T>
+    template <class T, int dim, T side>
     class Hypercube
     {
     public:
-        constexpr Hypercube(T dim, T side) : dimension_(dim), side_length_(side) {}
-
         constexpr T get_volume() const
         {
-            if (dimension_ < 0)
+            if (dim < 0)
                 return -1;
             T volume = 0;
-            for (T i = 0; i < dimension_; ++i)
-                volume = (i == 0) ? side_length_ : volume * side_length_;
+            for (int i = 0; i < dim; ++i)
+                volume = (i == 0) ? side : volume * side;
             return volume;
         }
-
-    private:
-        int dimension_;
-        T side_length_;
     };
 }
 
 int main()
 {
-    constexpr cpplab::Hypercube<int> a(4, 2);
-    constexpr cpplab::Hypercube<double> b(0, 3.14);
-    constexpr cpplab::Hypercube<float> c(-1, 2.71);
+    constexpr cpplab::Hypercube<int, 4, 2> a;
+    constexpr cpplab::Hypercube<double, 0, 3.14> b;
+    constexpr cpplab::Hypercube<float, -1, 2.71f> c;
 
     static_assert(a.get_volume() == 16, "Assertion failed");
     static_assert(b.get_volume() == 0, "Assertion failed");
