27e3a060429127794d090953f7287bff5e3ea623
xen <selegakamil@gmail.com>
2022-11-17 16:58:59 +0100

    Add Pixel struct to test if a copy is created when using emplace_back

--- Lista3/zad2.cpp
+++ Lista3/zad2.cpp
@@ -1,15 +1,40 @@
 #include "vector.hpp"
 
+struct Pixel
+{
+    int r;
+    int g;
+    int b;
+
+    Pixel()
+    {
+    }
+    Pixel(int r, int g, int b)
+        : r(r), g(g), b(b)
+    {
+        std::cout << "default constructor" << std::endl;
+    }
+    Pixel(const Pixel &other)
+    {
+        r = other.r;
+        g = other.g;
+        b = other.b;
+        std::cout << "copy constructor" << std::endl;
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const Pixel &p)
+    {
+        os << "(" << p.r << ", " << p.g << ", " << p.b << ")";
+        return os;
+    }
+};
+
 int main()
 {
-    cpplab::vector<int> a{1, 2, 3};
-    std::cout << a << std::endl;
+    cpplab::vector<Pixel> a;
 
-    a.push_back(7);
-    std::cout << a << std::endl;
-
-    a.emplace_back(9, 10, 11);
-    std::cout << a << std::endl;
+    a.emplace_back(1, 2, 3);
+    a.emplace_back(4, 5, 6);
 
     return 0;
 }
