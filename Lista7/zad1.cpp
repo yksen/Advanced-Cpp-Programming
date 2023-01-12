07489d85430b5c9fa307b63b73c645eca38db38a
xen <selegakamil@gmail.com>
2023-01-12 16:27:19 +0100

    Add constructor, destructor and start method

--- Lista7/zad1.cpp
+++ Lista7/zad1.cpp
@@ -9,9 +9,20 @@ namespace cpplab
 {
     class ThreadPool
     {
+    public:
         std::vector<std::thread> threads;
-        size_t tasksFinished;
+        size_t tasksFinished{0};
         double sumOfResults{0};
+
+        ThreadPool(size_t threadCount)
+        {
+            start(threadCount);
+        }
+
+        ~ThreadPool()
+        {
+            stop();
+        }
 
         void add_task(std::function<double()> task)
         {
@@ -22,6 +33,13 @@ namespace cpplab
             return sumOfResults / tasksFinished;
         }
 
+    private:
+        void start(size_t threadCount)
+        {
+            for (size_t i = 0; i < threadCount; ++i)
+                threads.emplace_back(std::thread());
+        }
+
         void stop()
         {
         }
@@ -30,4 +48,5 @@ namespace cpplab
 
 int main()
 {
+    cpplab::ThreadPool pool(10);
 }
