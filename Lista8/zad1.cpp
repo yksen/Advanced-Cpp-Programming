0214b76c30784cad3959cc58f3f453f59378cb97
xen <selegakamil@gmail.com>
2023-01-27 18:07:04 +0100

    Create Engine class with it's constructor and needed member variables

--- Lista8/zad1.cpp
+++ Lista8/zad1.cpp
@@ -1,12 +1,15 @@
+#include <chrono>
 #include <iostream>
 #include <memory>
+#include <vector>
 
 namespace cpplab
 {
     class FuelTank
     {
     public:
-        FuelTank(uint32_t fuel) : fuel(fuel) {}
+        FuelTank(uint32_t fuel)
+            : fuel(fuel) {}
 
         uint32_t refuel(uint32_t fuelRequested)
         {
@@ -15,6 +18,18 @@ namespace cpplab
 
     private:
         uint32_t fuel;
+    };
+
+    class Engine
+    {
+    public:
+        Engine(uint32_t fuelConsumption, uint32_t refuelInterval)
+            : fuelConsumption(fuelConsumption), refuelInterval(refuelInterval) {}
+
+    private:
+        uint32_t fuelConsumption;
+        std::chrono::seconds refuelInterval;
+        std::vector<std::shared_ptr<FuelTank>> fuelTanks;
     };
 }
 
