ee3be9ddc65c6e28a4b6f2795f62073c56b2ff78
xen <selegakamil@gmail.com>
2023-01-28 22:01:20 +0100

    Run refueling thread in engine's constructor, join the thread in destructor

--- Lista8/zad1.cpp
+++ Lista8/zad1.cpp
@@ -1,6 +1,8 @@
 #include <chrono>
 #include <iostream>
 #include <memory>
+#include <mutex>
+#include <thread>
 #include <vector>
 
 namespace cpplab
@@ -13,28 +15,53 @@ namespace cpplab
 
         uint32_t refuel(uint32_t fuelRequested)
         {
+            std::lock_guard<std::mutex> lock(mutex);
             return fuel < fuelRequested ? 0 : (fuel -= fuelRequested, fuelRequested);
         }
 
     private:
         uint32_t fuel;
+        std::mutex mutex;
     };
 
     class Engine
     {
     public:
-        Engine(uint32_t fuelConsumption, uint32_t refuelInterval)
-            : fuelConsumption(fuelConsumption), refuelInterval(refuelInterval) {}
+        Engine(uint32_t fuelConsumption, std::chrono::seconds refuelInterval)
+        {
+            refuelThread = std::thread([this, fuelConsumption, refuelInterval]
+            {
+                do
+                {
+                    std::this_thread::sleep_for(refuelInterval);
+                    std::lock_guard<std::mutex> lock(mutex);
+                    for (auto fuelTank = fuelTanks.begin(); fuelTank != fuelTanks.end();)
+                    {
+                        auto fuel = (*fuelTank)->refuel(fuelConsumption);
+                        if (fuel == 0)
+                            fuelTanks.erase(fuelTank);
+                        else
+                            fuelTank = fuelTanks.end();
+                    }
+                } while (!fuelTanks.empty());
+            });
+        }
+
+        ~Engine()
+        {
+            refuelThread.join();
+        }
 
         void addFuelTank(std::shared_ptr<FuelTank> fuelTank)
         {
+            std::lock_guard<std::mutex> lock(mutex);
             fuelTanks.push_back(fuelTank);
         }
 
     private:
-        uint32_t fuelConsumption;
-        std::chrono::seconds refuelInterval;
         std::vector<std::shared_ptr<FuelTank>> fuelTanks;
+        std::thread refuelThread;
+        std::mutex mutex;
     };
 }
 
