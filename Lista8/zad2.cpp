97bbf24cfeb2e4cb0257400ddcaba7b5f7d205d6
xen <selegakamil@gmail.com>
2023-01-29 14:38:44 +0100

    Add a destructor, get method and operator overloads, delete copy constructor and copy assignment operator

--- Lista8/zad2.cpp
+++ Lista8/zad2.cpp
@@ -1,4 +1,6 @@
-#include <concepts>
+#include <iostream>
+#include <memory>
+#include <string>
 
 namespace cpplab
 {
@@ -7,23 +9,42 @@ namespace cpplab
     {
     public:
         unique_ptr(T *pointer) : pointer(pointer) {}
+        unique_ptr(const unique_ptr &) = delete;
+        unique_ptr &operator=(const unique_ptr &) = delete;
+        ~unique_ptr() noexcept { delete pointer; }
+
+        T *get() const { return pointer; }
+        T *operator->() const { return pointer; }
+        T &operator*() const { return *pointer; }
 
     private:
         T *pointer;
     };
 
     template <typename T>
-        requires std::is_pointer_v<T>
-    class non0_ptr
+    class non0_ptr : public unique_ptr<T>
     {
     public:
-        non0_ptr(T *pointer) : pointer(pointer) {}
-
-    private:
-        T *pointer;
+        non0_ptr(T *pointer) : unique_ptr<T>(pointer)
+        {
+            if (pointer == nullptr)
+                throw std::invalid_argument("cpplab::non0_ptr cannot be initialized with nullptr");
+        }
     };
 }
 
 int main()
 {
+    cpplab::unique_ptr<int> up{new int(3)};
+    cpplab::unique_ptr<int> up2{nullptr};
+
+    cpplab::non0_ptr<int> np{new int(7)};
+    try
+    {
+        cpplab::non0_ptr<int> np2{nullptr};
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
 }
