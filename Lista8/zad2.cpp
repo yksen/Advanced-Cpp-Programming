2dd70e4ac059fc86ec1ec9d21ab02427db0d2498
xen <selegakamil@gmail.com>
2023-01-29 14:39:23 +0100

    Add release, reset and swap

--- Lista8/zad2.cpp
+++ Lista8/zad2.cpp
@@ -13,9 +13,24 @@ namespace cpplab
         unique_ptr &operator=(const unique_ptr &) = delete;
         ~unique_ptr() noexcept { delete pointer; }
 
-        T *get() const { return pointer; }
-        T *operator->() const { return pointer; }
-        T &operator*() const { return *pointer; }
+        T *get() const noexcept { return pointer; }
+        T *operator->() const noexcept { return pointer; }
+        T &operator*() const noexcept { return *pointer; }
+
+        T *release() noexcept
+        {
+            pointer = nullptr;
+            return pointer;
+        }
+        void reset(T *pointer) noexcept
+        {
+            delete this->pointer;
+            this->pointer = pointer;
+        }
+        void swap(unique_ptr &other) noexcept
+        {
+            std::swap(pointer, other.pointer);
+        }
 
     private:
         T *pointer;
@@ -30,6 +45,11 @@ namespace cpplab
             if (pointer == nullptr)
                 throw std::invalid_argument("cpplab::non0_ptr cannot be initialized with nullptr");
         }
+
+        void release()
+        {
+            throw std::invalid_argument("cpplab::non0_ptr cannot be released");
+        }
     };
 }
 
@@ -42,6 +62,18 @@ int main()
     try
     {
         cpplab::non0_ptr<int> np2{nullptr};
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    up.release();
+    std::cout << up.get() << std::endl;
+
+    try
+    {
+        np.release();
     }
     catch (const std::exception &e)
     {
